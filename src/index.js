3a471df457653b28f74b599cf4020c69152439d6
Paloma Falchi <palomapfalchi@gmail.com>
2021-12-16 10:29:19 -0300

    corrigido src das imagens no arquivo index.js e exclusao de arquivos vazios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // vetor de objetos de cursos que estavam listados no html
 const cursos = [
-    {nome: "Desenvolvimento Web", img: `<img src="../imagens/ilustra-web.png">`, descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti."},
-    {nome: "Marketing Digital", img: `<img src="../imagens/ilustra-marketing.png" >`, descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti."},
-    {nome: "Consultorio UX", img: `<img src="../imagens/ilustra-ux.png">`, descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti."}
+    {nome: "Desenvolvimento Web", img: `<img src="imagens/ilustra-web.png">`, descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti."},
+    {nome: "Marketing Digital", img: `<img src="imagens/ilustra-marketing.png" >`, descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti."},
+    {nome: "Consultorio UX", img: `<img src="imagens/ilustra-ux.png">`, descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti."}
 ]
 
 // função para listar os cursos e jogar no html cursos que já estavam listados
